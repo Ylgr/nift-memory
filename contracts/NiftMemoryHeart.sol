e6b3c53df9e7018dcb4cebe295e7267a4a8ad4ab
ylgr <quyen.luminous@gmail.com>
2021-07-05 16:48:08 +0700

    (+) nift heart

--- contracts/NiftMemoryHeart.sol
+++ contracts/NiftMemoryHeart.sol
@@ -9,45 +9,46 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract NiftMemoryTreasure is ERC721URIStorage, Ownable {
+contract NiftMemoryHeart is ERC721URIStorage, Ownable {
 
-    constructor() ERC721("Nift memory artifact", "NIFT-ARTIFACT") {}
+    constructor() ERC721("Nift memory heart", "NIFT-HEART") {}
 
-    IERC20 dust = IERC20("");
-    IERC721 treasure = IERC721("");
+    IERC20 dust = IERC20(0xaae5fc57aE9B2702e165224bc7b4f700ba698b22);
+    IERC721 treasure = IERC721(0xE3864Fb24851EA437043Ae62104dF4692e11B8b1);
+
+    uint8 tokenId = 99;
 
     event Craft(address addr, uint256 time);
     event Revert(address addr, uint256 time);
     event WithdrawDust(uint256 amount, address receiver, uint256 time);
 
     function craft() public {
-        treasure.transferFrom(msg.sender, this, 1);
-        treasure.transferFrom(msg.sender, this, 2);
-        treasure.transferFrom(msg.sender, this, 3);
+        treasure.transferFrom(msg.sender, address(this), 1);
+        treasure.transferFrom(msg.sender, address(this), 2);
+        treasure.transferFrom(msg.sender, address(this), 3);
 
-        dust.transferFrom(msg.sender, this, 100 * 1e18);
+        dust.transferFrom(msg.sender, address(this), 100 * 1e18);
 
-        _mint(msg.sender,1);
+        _mint(msg.sender,tokenId);
 
-        _setTokenURI(1, "https://ylgr.github.io/nift-memory/artifact/artifact.json");
+        _setTokenURI(tokenId, "https://ylgr.github.io/nift-memory/heart/heart.json");
 
         emit Craft(msg.sender, block.timestamp);
     }
 
     function revert() public {
-        require(_owners[1] == msg.sender, "Not own artifact");
-        _burn(1);
+        require(ownerOf(tokenId) == msg.sender, "Not own heart");
+        _burn(tokenId);
 
-        treasure.transferFrom(this, msg.sender, 1);
-        treasure.transferFrom(this, msg.sender, 2);
-        treasure.transferFrom(this, msg.sender, 3);
+        treasure.transferFrom(address(this), msg.sender, 1);
+        treasure.transferFrom(address(this), msg.sender, 2);
+        treasure.transferFrom(address(this), msg.sender, 3);
 
         emit Revert(msg.sender, block.timestamp);
     }
 
     function withdrawDust() public onlyOwner {
-        IERC20 token = IERC20(_token);
-        uint256 amount = token.balanceOf(address(this));
+        uint256 amount = dust.balanceOf(address(this));
 
         require(
             amount > 0,
@@ -55,7 +56,7 @@ contract NiftMemoryTreasure is ERC721URIStorage, Ownable {
         );
 
         require(
-            token.transfer(owner(), amount),
+            dust.transfer(owner(), amount),
             "Token transfer fail"
         );
 
--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -1,7 +1,9 @@
 const NiftMemoryDust = artifacts.require("./NiftMemoryDust.sol");
 const NiftMemoryTreasure = artifacts.require("./NiftMemoryTreasure.sol");
+const NiftMemoryHeart = artifacts.require("./NiftMemoryHeart.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(NiftMemoryDust);
   deployer.deploy(NiftMemoryTreasure);
+  deployer.deploy(NiftMemoryHeart);
 };
